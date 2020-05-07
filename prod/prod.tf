13c9244158c5b04c7b42135882694c211482ad1c
Yann Toqué <toqueyann@gmail.com>
2020-05-07 11:02:34 +0200

    AutoscalingGroup

--- prod/prod.tf
+++ prod/prod.tf
@@ -60,50 +60,10 @@ resource "aws_security_group" "prod_web" {
 }
 
 /*
-instance with NGINX Open Source Certified by Bitnami subscription
-*/
-resource "aws_instance" "prod_web" {
-  count = 2
-
-  ami = "ami-0a0f02941b5882dd8"
-  instance_type = "t2.nano"
-
-  vpc_security_group_ids = [
-    aws_security_group.prod_web.id
-  ]
-
-  tags = {
-    "Terraform" = "true"
-  }
-}
-
-/*
-Decoupling the creation of the IP and it's assigment
-Scaling : 
-syntax accepted  aws_instance.prod_web[0].id
-or aws_instance.prod_web.0.id
- 
-*/
-resource "aws_eip_association" "prod_web" {
-  instance_id   = aws_instance.prod_web.0.id
-  allocation_id = aws_eip.prod_web.id 
-}
-
-  
-
-/*EIP*/
-resource "aws_eip" "prod_web" {
-  tags = {
-    "Terraform" = "true"
-  }
-}
-
-/*
 loadbalancer*/
 resource "aws_elb" "prod_web" {
   name            = "prod-web"
-  instances       = aws_instance.prod_web.*.id
-  subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id ]
+  subnets         = [aws_default_subnet.default_az1.id,aws_default_subnet.default_az2.id]
   security_groups = [aws_security_group.prod_web.id]
 
   listener{
@@ -116,4 +76,44 @@ resource "aws_elb" "prod_web" {
   tags = {
     "Terraform" = "true"
   }
+}
+
+/*Autoscaling group
+from https://www.terraform.io/docs/providers/aws/r/autoscaling_group.html*/
+resource "aws_launch_template" "prod_web" {
+  name_prefix   = "prod-web"
+  image_id      = "ami-0a0f02941b5882dd8"
+  instance_type = "t2.micro"
+
+  tags = {
+    "Terraform" = "true"
+  }
+}
+/*
+ vpc_zone_identifier A list of subnet IDs to launch resources in.
+*/
+resource "aws_autoscaling_group" "prod_web" {
+  availability_zones  = ["eu-west-3a","eu-west-3b"]
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id,aws_default_subnet.default_az2.id]
+  desired_capacity    = 1
+  max_size            = 1
+  min_size            = 1
+
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+
+  tag {
+      key                 = "Terraform"
+      value               = "true"
+      propagate_at_launch = true
+  }
+}
+/**
+To connect the elb to the autoscaling group
+*/
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web.id
 }
